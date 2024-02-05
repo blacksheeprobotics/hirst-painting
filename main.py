6eb59bfb2aad4ff1337ba556d94fe2409364a6c2
blacksheeprobotics <blacksheeprobotics@gmail.com>
2024-02-05 13:54:26 -0500

    Removed cologram code

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-###This code will not work in repl.it as there is no access to the colorgram package here.###
-##We talk about this in the video tutorials##
-import colorgram
 import turtle as t
 import random
-
-# rgb_colors = []
-# colors = colorgram.extract('image.jpg', 30)
-# for color in colors:
-#     red = color.rgb.r
-#     green = color.rgb.g
-#     blue = color.rgb.b
-#     rgb_colors.append((red, green, blue))
-# print(rgb_colors)
 
 color_list = [(202, 164, 110),
               (236, 239, 243),
